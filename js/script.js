de19b8f530094dd3f1a4016bb6e2ced12a8a48ac
AndrielCardoso <andrielvcardoso2@gmail.com>
2021-06-28 10:17:32 -0300

    Front-end studies

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 5000, // tempo de transição dos slides
+});
+
+new SimpleSlide({
+    slide: 'imagens', // nome do atributo data-slide="principal"
+    nav: true, // se deve ou não mostrar a navegação
+    auto: true, // se o slide deve passar automaticamente
+    time: 5000, // tempo de transição dos slides
+  });
+}
+
+if (window.SimpleAnime) {
+new SimpleAnime ({
+
+})
+}
